35c33736afdd3eabe2fde9e222518cafe588a686
Viktor <zvictz@gmail.com>
2026-05-09 03:28:43 +0100

    Update 2026-05-09  3:28:43.00

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -52,7 +52,7 @@ export async function generateMetadata({
     // Посилання на іконки (мають бути у папці /public)
     icons: {
       icon: [
-        { url: '/favicon.ico' },
+        // { url: '/favicon.ico' },
         { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
         { url: '/icon-512.png', sizes: '512x512', type: 'image/png' }, // Додано для Splash Screen
       ],
