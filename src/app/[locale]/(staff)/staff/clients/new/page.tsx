49b98380d9b8bdab3b18cc08845d6bb8431dd642
Viktor <zvictz@gmail.com>
2026-05-21 11:14:01 +0100

    Update 2026-05-21 11:14:01.56

--- src/app/[locale]/(staff)/staff/clients/new/page.tsx
+++ src/app/[locale]/(staff)/staff/clients/new/page.tsx
@@ -5,7 +5,7 @@ import ClientForm from "@/components/staff/ClientForm"
 
 export default function NewClientPage() {
   return (
-    <div className="container mx-auto py-10 px-4">
+    <div className="container mx-auto py-2 px-2">
       {/* No initialData or id passed here. 
           ClientForm will correctly show "New Recruit" 
       */}
